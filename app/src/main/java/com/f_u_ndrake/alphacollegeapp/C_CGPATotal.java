147855182f793d3f1462bdda16b1ae775078889c
FUNDrake <adarshpalande@gmail.com>
2021-07-29 16:23:45 +0530

    Final Done

---
+++ app/src/main/java/com/f_u_ndrake/alphacollegeapp/C_CGPATotal.java
@@ -0,0 +1,14 @@
+package com.f_u_ndrake.alphacollegeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class C_CGPATotal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c__c_g_p_a_total);
+    }
+}
